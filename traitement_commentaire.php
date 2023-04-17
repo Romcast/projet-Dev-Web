37925339e87c2f79ace386b235405773a669031a
ren simon <118524337+Aaaren1@users.noreply.github.com>
2023-04-17 23:26:23 +0200

    Add files via upload

--- traitement_commentaire.php
+++ traitement_commentaire.php
@@ -1,6 +1,6 @@
 <?php
 // Récupérer les données du formulaire
-$page_id = $_POST['page_id'];
+//$page_id = $_POST['page_id'];
 $username = $_POST['username'];
 $comment = $_POST['comment'];
 $note = $_POST['note'];
@@ -14,11 +14,6 @@ $dbname="miam";
 $dbco = new PDO("mysql:host=$serveur;dbname=$dbname",$user,$pass);
 $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-// Vérifier la connexion à la base de données
-if($mysqli->connect_error) {
-    die("Erreur de connexion à la base de données: " . $mysqli->connect_error);
-}
-
 try{
     //On crée une table commentaire
     $commentaire = "CREATE TABLE IF NOT EXISTS commentaire(
@@ -27,18 +22,34 @@ try{
         commentaire VARCHAR(5000),
         note INT
         )";
+    if($dbco->exec($commentaire)==true){
+        echo "la table commentaire a bien été créée; ";
+    }
+    else{
+        echo "la table commentaire existe deja ou il y a une erreur; ";
+    }
+    
+    // Insérer le commentaire dans la base de données avec l'ID de la page
+    $query = "INSERT INTO commentaire (nom, commentaire, note) VALUES ( '$username', '$comment', $note)";
+    if($dbco->exec($query)==true){
+        echo "la requete a bien été exécutée; ";
+    }
+    else{
+        echo "une erreur s'est produite lors de la requete; ";
+    }
+}
 
-// Insérer le commentaire dans la base de données avec l'ID de la page
-$query = "INSERT INTO commentaire (page_id, username, commentaire, note) VALUES ('$page_id', '$username', '$comment', '$note')";
-$result = $mysqli->query($query);}
+catch(PDOException $e){
+    echo 'Erreur : '.$e->getMessage();
+}
+$dbco= null;
 
 // Vérifier si l'insertion a réussi
-if ($result) {
-    header("Location: formulaire_commentaires.php"); // Rediriger vers la page du formulaire de commentaire
-} else {
-    echo "Erreur lors de l'ajout du commentaire : " . $mysqli->error;
+// if ($result) {
+//     header("Location: formulaire_commentaires.php"); // Rediriger vers la page du formulaire de commentaire
+// } else {
+//     echo "Erreur lors de l'ajout du commentaire : " . $mysqli->error;
 
-}
-// Fermer la connexion à la base de données
-$mysqli->close();
+// }
+
 ?>
