4fb929edddd5a171c0635a37c00773d3dbe11417
Romcast <129684627+Romcast@users.noreply.github.com>
2023-04-20 23:33:56 +0200

    Add files via upload

--- sqlConn.php
+++ sqlConn.php
@@ -16,24 +16,28 @@ if ($conn->connect_error) {
 };
 
 
-if ($conn->query("SELECT * FROM utilisateur") == false){
-  $utilisateur = "CREATE TABLE IF NOT EXISTS utilisateur(
-    id_user INT UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    email VARCHAR(100),
-    password VARCHAR(30),
-    nom VARCHAR(50) DEFAULT '',
-    prenom VARCHAR(50) DEFAULT '',
-    image VARCHAR(50) DEFAULT 'imageProfil/utilisateur.png',
-    date_creation DATE DEFAULT '',
-    administrateur INTEGER DEFAULT 0,
-    ban INTEGER DEFAULT 0,
-    note_moy FLOAT DEFAULT 0.0,
-    util_semaine INTEGER DEFAULT 0
-    )";
-  
-  $admin = "INSERT INTO utilisateur(email,password,administrateur) VALUES ('admin',0000,1)";
+
+$utilisateur = "CREATE TABLE IF NOT EXISTS utilisateur(
+  id_user INT UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  email VARCHAR(100),
+  password VARCHAR(30),
+  nom VARCHAR(50) DEFAULT '',
+  prenom VARCHAR(50) DEFAULT '',
+  image VARCHAR(50) DEFAULT 'imageProfil/utilisateur.png',
+  date_creation DATE DEFAULT '0000-00-00',
+  administrateur INTEGER DEFAULT 0,
+  ban INTEGER DEFAULT 0,
+  note_moy FLOAT DEFAULT 0.0,
+  util_semaine INTEGER DEFAULT 0
+  )";
+
   $conn->query($utilisateur);
-  $conn->query($admin);
+  $util = $conn->query("SELECT * FROM utilisateur");
+  if ($util->fetch_assoc() == NULL){
+    $admin = "INSERT INTO utilisateur(email,password,administrateur) VALUES ('admin',0000,1)";
+    $conn->query($admin);
   }
+
+
 
 ?>
