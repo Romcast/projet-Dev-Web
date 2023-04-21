59b513d72196076c389dfb68032cbf0165d504ac
Romcast <129684627+Romcast@users.noreply.github.com>
2023-04-21 21:59:48 +0200

    Add files via upload

--- utilSemaine.php
+++ utilSemaine.php
@@ -4,7 +4,7 @@
             <a href="visuelProfil.php?id_user=<?php echo $id_user ?>">
             <div class="carre_util_semaine">
                 <div class="miniprofil">
-                <img class="image_profil" src= <?php echo $image ?> alt="introuvable" width="100" height="100" >
+                <img src= <?php echo $image ?> alt="introuvable" width="100" height="100" >
                 <div class="description_util_semaine"> 
                      <label><?php echo $email ?></label> <br>
                      <label class = "infos_util_semaine"> <?php echo $nom . ' ' . $prenom ?> </label> <br>
@@ -12,9 +12,24 @@
                      <label class = "infos_util_semaine"> Créé le <?php echo $date ?> </label>
                 </div>
                 </div>
+                <a href="visuelrecette.php?id=<?php echo $id_recette?>">
                 <div class="recette_util_semaine ">
-                        <h3> Meilleure recette <h3>
+                        <label style="font-size:20">Meilleure recette :</label>
+                           <img src= <?php echo $photo_recette ?> alt="introuvable" width="50" height="50" >
+                            <div class="description_util_semaine"> 
+                                <label style="font-size:30"><?php echo $nom_recette ?></label>
+                                <span class='tab'></span>
+                                <label> Note : <?php echo $note_recette ?> </label>
+                                <span class='tab'></span>
+                                <label> <?php echo $type ?> </label> 
+                                <label> pour <?php echo $nombre_personnes ?> </label>
+                                <span class='tab'></span>
+                                <label> <?php echo $difficulte ?> </label> 
+                                <span class='tab'></span>
+                                <label> Cout : <?php echo $cout ?> € </label>
+                            </div>
                 </div>
+                </a>
                 
             </div>
             </a>
--- utilSemaineSql.php
+++ utilSemaineSql.php
@@ -5,12 +5,25 @@ if ($user->num_rows > 0) {
     $row = $user->fetch_assoc();
     $id_user = $row['id_user'];
     $email = $row['email'];
-    $nom = $row['nom'];
-    $prenom = $row['prenom'];
-    $note_moy = $row['note_moy'];
-    $image = $row['image'];
-    $date = $row['date_creation'];
-    include('utilSemaine.php');
+    $recette = $conn->query("SELECT * FROM form_recette WHERE auteur = '$email' AND note = (SELECT MAX(note) FROM form_recette WHERE auteur = '$email')");
+    if ($recette != false && $recette->num_rows > 0){
+        $nom = $row['nom'];
+        $prenom = $row['prenom'];
+        $note_moy = $row['note_moy'];
+        $image = $row['image'];
+        $date = $row['date_creation'];
+
+        $row2 = $recette->fetch_assoc();
+        $id_recette = $row2['id'];
+        $photo_recette = $row2['photo'];
+        $nom_recette = $row2['nom'];
+        $type = $row2['type_repas'];
+        $difficulte = $row2['difficulte'];
+        $nombre_personnes = $row2['nombre_personnes'];
+        $note_recette = $row2['note'];
+        $cout = $row2['cout_recette'];
+        include('utilSemaine.php');
+    }
 }
 $conn->close();
 ?>
