726c0d6cb4efdc9e0135eff88ac3e2383fc17fb3
Romcast <129684627+Romcast@users.noreply.github.com>
2023-04-21 21:10:44 +0200

    Add files via upload

--- headerProfil.php
+++ headerProfil.php
@@ -1,5 +1,5 @@
-<img src=<?php echo $_SESSION['image'] ?> alt="Introuvable" width="30" height="30" onclick="window.location='profil.php'">
-<label onclick="window.location='profil.php'"><?php echo $_SESSION['email'] ?> </label>
+<img src=<?php echo $_SESSION['image'] ?> alt="Introuvable" width="30" height="30" onclick="window.location='visuelProfil.php?id_user=<?php echo $_SESSION['id_user']?>'">
+<label onclick="window.location='visuelProfil.php?id_user=<?php echo $_SESSION['id_user']?>'"><?php echo $_SESSION['email'] ?> </label>
 <br>
 <button onclick="window.location='nouvellerecette.php'">Créer une nouvelle recette</button>
 <form action="deconnexion.php">
