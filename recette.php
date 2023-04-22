6c91b63012d2f0eb1218bc712fe9b3e8f269225a
ren simon <118524337+Aaaren1@users.noreply.github.com>
2023-04-22 12:44:47 +0200

    Add files via upload

--- recette.php
+++ recette.php
@@ -1,88 +1,17 @@
 <?php 
-        // $auteur = $_POST['auteur']; 
+    if(!isset($_SESSION)){
         session_start();
-        $nom = $_POST['nom'];
-        $type= $_POST['type'];
-        $nombre = $_POST['nb_personnes'];
-        $difficulte = $_POST['difficulte'];
-        $nouvelleQuantite= $_POST['nouvelleQuantite'];
-        $nouvelleUnite=$_POST['nouvelleUnite'];
-        $nouvelIngredient=$_POST['nouvelIngredient'];
-        $nouvelleEtape=$_POST['nouvelleEtape'];
-        $auteur=$_SESSION['email'];
-
-        
-        
+    }
+    $nom = $_POST['nom'];
+    $type= $_POST['type'];
+    $nombre = $_POST['nb_personnes'];
+    $difficulte = $_POST['difficulte'];
+    $nouvelleQuantite= $_POST['nouvelleQuantite'];
+    $nouvelleUnite=$_POST['nouvelleUnite'];
+    $nouvelIngredient=$_POST['nouvelIngredient'];
+    $nouvelleEtape=$_POST['nouvelleEtape'];
+    $auteur=$_SESSION['email'];
         $conseil = $_POST['conseils'];
-
-
-        
-        
-
-
-        $html = '<h1>' . $nom . '</h1>';
-        $html .= '<p><strong> Type de recette:</strong> ' .$type . '</p>';
-        $html .= '<p><strong>Nombre de personnes:</strong> ' . $nombre . '</p>';
-        $html .= '<p><strong>Difficulté:</strong> ' . $difficulte . '</p>';
-
-        //$html .= $photo;
-        $html .= '<h2>Ingrédients:</h2>';
-        $html .= '<ul>';
-
-        $size_ing=sizeof($nouvelIngredient);
-        for ($i=0; $i<$size_ing; $i++){
-            $html .= '<li>' . $nouvelleQuantite[$i] ." ". $nouvelleUnite[$i]." de " .$nouvelIngredient[$i].   '</li>';
-        }
-        $html .= '</ul>';
-        
-
-        $html .= '<h2>phase technique:</h2>';
-        $html .= '<ul>';
-        $size_etapes = sizeof($nouvelleEtape);
-        for ($i=0; $i<$size_etapes; $i++){
-                $html .= '<li>' . $nouvelleEtape[$i].   '</li>';
-        }
-        $html .= '</ul>';
-        
-
-
-        $html .= '<h2>Derniers conseils:</h2>';
-        $html .= '<p>' . $conseil . '</p>';
-
-        //print_r($nouvelleEtape);
-        //echo strip_tags($html);
-        //header("Location:merciRecette.html");
-
-        //afficher la photo
-        // $type=exif_imagetype($photo);
-
-        // if($type ==IMAGETYPE_JPEG){
-            
-        //     $image=imagecreatefromjpeg($photo);
-        //     header('Content-Type: image/jpeg');
-        //     imagejpeg($image);
-            
-        // }
-        // elseif($type ==IMAGETYPE_JPG){
-        //     $image=imagecreatefromjpg($photo);
-        //     header('Content-Type: image/jpg');
-        //     imagejpg($image);
-        // }
-        
-        // elseif($type ==IMAGETYPE_JPG){
-        //     $image=imagecreatefrompng($photo);
-        //     header('Content-Type: image/png');
-        //     imagepng($image);
-        // }
-
-        // else{
-        //     echo "erreur type";
-        // }
-        
-        // //readfile($photo);
-
-
-
 
     $serveur = "localhost";
     $dbname = "miam";
@@ -154,9 +83,11 @@
             recette_id iNT UNSIGNED,
             FOREIGN KEY(id) REFERENCES form_recette(id) ON DELETE CASCADE
             )";
-        $dbco->exec($form_ingredient);
-        echo " la table ingredient a ete cree; ";
+        if(!$dbco->exec($form_ingredient)){
+            echo " erreur création table ingrédient ou tbale déjà existante; ";
+        }
 
+        $size_ing=sizeof($nouvelIngredient);
         for ($i=0; $i<$size_ing; $i++){
             $quantite=  $nouvelleQuantite[$i] ;
             $ingredient= $nouvelIngredient[$i];
@@ -167,8 +98,10 @@
             //
             $entree_ingredient="INSERT INTO form_ingredient( nom, quantite, unite, cout, recette_id)
             VALUES('$ingredient', $quantite ,'$unite', NULL,$id_recette)";
-            $dbco->exec($entree_ingredient);
-             $ingredient_id = $dbco->lastInsertId();
+            if(!$dbco->exec($entree_ingredient)){
+                echo " erreur entrée dans table ingredient; ";
+            }
+            $ingredient_id = $dbco->lastInsertId();
             if($ingredient_existe->rowCount()==1){
                 //si l'ingredient existe dans la table on recupere son prix pour mettre a jour le prix du nouvel ingredient inseré dans la table
                 $ingredient_existe=$ingredient_existe->fetch();
@@ -184,10 +117,7 @@
                     $update_cout_recette=$dbco->prepare('UPDATE form_recette SET cout_recette=? WHERE id=?');
                     $update_cout_recette->execute(array($cout_recette,$id_recette));
                 }
-               
             }
-            //
-            echo 'ingredient '. $i+1 .' enregistré ';
         }
 
         $form_etape=  "CREATE TABLE IF NOT EXISTS form_etape(
@@ -196,23 +126,21 @@
             recette_id iNT UNSIGNED,
             FOREIGN KEY(id) REFERENCES form_recette(id) ON DELETE CASCADE
             )";
-        $dbco->exec($form_etape);
-        echo " la table etape a ete cree; ";
-
+        if(!$dbco->exec($form_etape)){
+            echo " erreur creation table etape ou table existante; ";
+        }
 
         foreach ($nouvelleEtape as $etape ){
             $entree_etape="INSERT INTO form_etape( etape, recette_id)
             VALUES('$etape', $id_recette )";
-            $dbco->exec($entree_etape);
-            echo 'etape '. $i .' enregistré ';
-        
+            if(!$dbco->exec($entree_etape)){
+                echo " erreur durant entrée table etape; ";
+            }
         
         }
         header("Location:home.php?message=creationsuccess");
     }
 
-        
-        
     catch(PDOException $e){
         echo 'Erreur : '.$e->getMessage();
     }
