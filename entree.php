a5630472266fb8132655de81933c6d64df184267
DreewF <129109041+DreewF@users.noreply.github.com>
2023-04-22 23:49:15 +0200

    Update and rename entree.html to entree.php

---
+++ entree.php
@@ -0,0 +1,39 @@
+<?php
+    require('header.php');
+    $servername ="localhost";
+    $username ="root";
+    $password = "";
+    $bdd = new PDO("mysql:host=$servername;dbname=miam;", $username, $password);
+    $nouvellerecette=$bdd->query('SELECT * FROM form_recette WHERE traitement="Validé" AND type_repas="Entrée"');
+?>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="liste.css" rel="stylesheet" type="text/css">
+        <title>Entrées</title>
+    </head>
+    <body>
+        <h1 class="titre">Entrées</h1>
+        <section class="recettes">
+            <?php
+                if($nouvellerecette->rowCount()>0){
+                    while($r=$nouvellerecette->fetch()){
+                        ?>
+                      <div class="recette">
+                            <img src="<?php echo isset($r['image']) ? $r['image'] : ''; ?>" alt="Image de la recette">
+                            <p><a href="visuelrecette.php?id=<?php echo $r['id']?>"><?php echo $r['nom'];?></a><?php echo " "; echo $r['auteur']?></p>
+                        </div>
+                        <?php
+                    }
+                }
+                else{
+                    ?>
+                    <p>Aucune recette trouvée</p>
+                    <?php
+                }
+            ?>
+
+        </section>
+    </body>
+</html>
