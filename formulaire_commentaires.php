76950499b002b959c75523d368d5d19e4037c687
DreewF <129109041+DreewF@users.noreply.github.com>
2023-04-22 23:06:35 +0200

    Update formulaire_commentaires.php

--- formulaire_commentaires.php
+++ formulaire_commentaires.php
@@ -4,25 +4,31 @@
     <link rel="stylesheet" href="commentaires.css"> <!-- Lien vers le fichier CSS -->
 </head>
 <body>
-<div class="comment-form">
-    <?php 
-        if (session_status() == PHP_SESSION_NONE) {
-            session_start();
-        }
-        
-        if(isset($_SESSION['ban']) && $_SESSION['ban'] == 0){
-    ?>
-        <h2>Ajouter un commentaire</h2>
-        <form action="traitement_commentaire.php?id=<?php echo $id; ?>" method="post">
-            <label for="comment">Commentaire :</label>
-            <textarea id="comment" name="comment" required></textarea>
-            <label for="note">Note(entre 1 et 5):</label>
-            <input type="number" id="note" name="note" min="1" max="5" required>
-            <button type="submit" name="submit">envoyer</button>
-        </form>
-    <?php } else { ?>
-        <div class="ban-message">Vous avez été banni. Comportez vous mieux la prochaine fois :D </div>
-    <?php } ?>
+    <br>
+    <br>
+    <button id="toggle-comments">Cacher les commentaires</button>
+    <div class="comment-form">
+
+        <?php 
+            if (session_status() == PHP_SESSION_NONE) {
+                session_start();
+            }
+            
+            if(isset($_SESSION['ban']) && $_SESSION['ban'] == 0){
+        ?>
+            <h2>Ajouter un commentaire</h2>
+            <form action="traitement_commentaire.php?id=<?php echo $id; ?>" method="post">
+                <label for="comment">Commentaire :</label>
+                <textarea id="comment" name="comment" required></textarea>
+                <label for="note">Note(entre 1 et 5):</label>
+                <input type="number" id="note" name="note" min="1" max="5" required>
+                <button type="submit" name="submit">envoyer</button>
+            </form>
+        <?php } else { ?>
+            <div class="ban-message">Vous avez été banni. Comportez vous mieux la prochaine fois :D </div>
+        <?php } ?>
+    </div>
+    <div class="comments">
     <h1>Commentaires</h1>
     <form method="post">
         <label for="tri">Trier par :</label>
@@ -33,7 +39,6 @@
         </select>
         <button type="submit">Trier</button>
     </form>
-    <div class="comments">
         <?php 
             if(isset($_SESSION['ban']) && $_SESSION['ban'] == 0){
                 if (isset($_GET['id'])) {
@@ -45,7 +50,23 @@
             } 
         ?>
     </div>
-    </div>
+    <script>
+        var button = document.getElementById("toggle-comments");
+        var commentsDiv = document.querySelector(".comments");
+        var commentFormDiv = document.querySelector(".comment-form");
+
+        button.addEventListener("click", function() {
+            if (commentsDiv.style.display === "none") {
+                commentsDiv.style.display = "block";
+                commentFormDiv.style.display = "block";
+                button.innerHTML = "Cacher les commentaires";
+            } else {
+                commentsDiv.style.display = "none";
+                commentFormDiv.style.display = "none";
+                button.innerHTML = "Afficher les commentaires";
+            }
+        });
+    </script>
 </body>
 </html>
 
