6506e6add25d8275cb57f5ec5171f3059b21a64c
DreewF <129109041+DreewF@users.noreply.github.com>
2023-04-22 17:55:06 +0200

    Update afficher_commentaire.php

--- afficher_commentaire.php
+++ afficher_commentaire.php
@@ -1,40 +1,82 @@
-<html>
-    <head>
-        <link href="afficher_commentaire.css" rel="stylesheet" type="text/css">
-    </head>
-    <body>
-        <?php
-            $serveur = "localhost";
-            $user = "root";
-            $pass = "";
-            $dbname="miam";
-            $recette_id=$_SESSION['recette_id'];
-            //On se connecte à la BDD
-            $dbco = new PDO("mysql:host=$serveur;dbname=$dbname",$user,$pass);
-            $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            try{
-                if (isset($_GET['id'])) {
-                    $statement=$dbco->prepare("SELECT mail_auteur,commentaire,note FROM commentaire WHERE recette_id=$recette_id");
-                    $statement->execute();
-                    $commentaires=$statement->fetchAll(PDO::FETCH_ASSOC);
-                    echo '<div>' . '<div class="ligne">' . '<div >' . 'auteur'. '</div>'.'<div>'. 'commentaire'.'</div>' . '<div >' . 'note' .'</div>'. '</div>'. '</div>';
-                    foreach($commentaires as $commentaire){
-                        echo '<div id="commentaires">' . '<div class="ligne">' . '<div class="auteur">' . $commentaire['mail_auteur']. '</div>'. '<div class="commentaire">' . $commentaire['commentaire'] .'</div>'. '<div class="note">' . $commentaire['note'] .'</div>'. '</div>'. '</div>';
-                    }
-                }
-    
-    
-            }
+<?php
+
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+$serveur = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "miam";
+
+// On se connecte à la BDD
+$dbco = new PDO("mysql:host=$serveur;dbname=$dbname;charset=utf8", $user, $pass);
+$dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+try{
+    if (isset($_SESSION['recette_id'])) {
+        $recette_id = $_SESSION['recette_id'];
+
+        $statement=$dbco->prepare("SELECT commentaire_id, mail_auteur, commentaire, note, date_creation FROM commentaire WHERE recette_id=:recette_id");
+        $statement->bindParam(':recette_id', $recette_id);
+        $statement->execute();
+        $commentaires=$statement->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach($commentaires as $commentaire){
+            // On récupère l'ID utilisateur correspondant à l'auteur du commentaire
+            $auteur=$commentaire['mail_auteur'];
+            $statement2=$dbco->prepare("SELECT id_user FROM utilisateur WHERE email=:email");
+            $statement2->bindParam(':email', $auteur);
+            $statement2->execute();
+            $id_user=$statement2->fetchColumn();
+
+            echo '<div style="border:2px solid black">' . "<a href='visuelProfil.php?id_user=$id_user'>",$commentaire['mail_auteur'],"</a>". ' a écrit: ' . $commentaire['commentaire'] . ' et a donné une note de ' . $commentaire['note'] . ' le ' . $commentaire['date_creation'] . '</div>';
+            echo "<button class='comment-delete' data-comment-id='" . $commentaire['commentaire_id'] . "'>Supprimer</button>";
+        }
+    }
+    // Récupérer la valeur du paramètre 'tri' dans l'URL, ou utiliser la valeur par défaut 'date'
+$tri = isset($_GET['tri']) ? $_GET['tri'] : 'date';
+
+// Construire la requête SQL pour récupérer les commentaires triés selon le paramètre 'tri'
+switch ($tri) {
+    case 'note':
+        $query = "SELECT * FROM commentaire WHERE recette_id = $recette_id ORDER BY note DESC";
+        break;
+    case 'date':
+        $query = "SELECT * FROM commentaire WHERE recette_id = $recette_id ORDER BY date_creation DESC";
+        break;
+    default:
+        $query = "SELECT * FROM commentaire WHERE recette_id = $recette_id";
+}
+
+}
+
+catch(PDOException $e){
+}
+
+$dbco=null;
+?>
 
 
-            catch(PDOException $e){
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+// Fonction pour supprimer un commentaire
+function deleteComment(commentId) {
+    console.log(commentId);
+    $.ajax({
+        type: 'POST',
+        url: 'supprimer_commentaire.php',
+        data: {commentId: commentId},
+        success: function(response) {
+            // Actualiser la liste des commentaires affichée sur la page
+            $('.comments').load('afficher_commentaire.php');
+        }
+    });
+}
 
-            }
-
-            $dbco=null;
-        ?>
-
-    </body>
-    
-</html>
-
+// Gérer le clic sur le bouton "supprimer" d'un commentaire
+$(document).on('click', '.comment-delete', function() {
+    var commentId = $(this).data('comment-id');
+    deleteComment(commentId);
+});
+</script>
