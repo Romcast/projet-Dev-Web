8269baaaecd51c075e4ee80234b4cc723fc646a9
DreewF <129109041+DreewF@users.noreply.github.com>
2023-04-22 17:56:22 +0200

    Update formulaire_commentaires.php

--- formulaire_commentaires.php
+++ formulaire_commentaires.php
@@ -4,30 +4,48 @@
     <link rel="stylesheet" href="commentaires.css"> <!-- Lien vers le fichier CSS -->
 </head>
 <body>
-    <div class="comment-form">
-    <h1>Commentaires</h1>
-    <div class="comments">
-        <?php 
-        if (isset($_GET['id'])) {
-            include_once("afficher_commentaire.php");
+<div class="comment-form">
+    <?php 
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
         }
-        else{
-            echo "erreur trasmission id depuis visuelrecette.php a formulaire_commentaires.php";
-        }
-        ?>
-    </div>
+        
+        if(isset($_SESSION['ban']) && $_SESSION['ban'] == 0){
+    ?>
         <h2>Ajouter un commentaire</h2>
         <form action="traitement_commentaire.php?id=<?php echo $id; ?>" method="post">
-            <!-- <input type="hidden" name="page_id" value="1"> Champ caché pour stocker l'ID de la page -->
-            <!-- <label for="username">Nom d'utilisateur :</label>
-            <input type="text" id="username" name="username" required> -->
             <label for="comment">Commentaire :</label>
             <textarea id="comment" name="comment" required></textarea>
             <label for="note">Note(entre 1 et 5):</label>
             <input type="number" id="note" name="note" min="1" max="5" required>
             <button type="submit" name="submit">envoyer</button>
         </form>
+    <?php } else { ?>
+        <div class="ban-message">Vous avez été banni. Comportez vous mieux la prochaine fois :D </div>
+    <?php } ?>
+    <h1>Commentaires</h1>
+    <form method="post">
+        <label for="tri">Trier par :</label>
+        <select id="tri" name="tri">
+            <option value="date">Date</option>
+            <option value="note">Note</option>
+            <option value="rien">Pas de filtre</option>
+        </select>
+        <button type="submit">Trier</button>
+    </form>
+    <div class="comments">
+        <?php 
+            if(isset($_SESSION['ban']) && $_SESSION['ban'] == 0){
+                if (isset($_GET['id'])) {
+                    include_once("afficher_commentaire.php");
+                }
+                else{
+                    echo "erreur transmission id depuis visuelrecette.php a formulaire_commentaires.php";
+                }
+            } 
+        ?>
     </div>
-    
+    </div>
 </body>
 </html>
+
