22a016e80fd4f7a0414de4b1d2d08b7642fa146c
sarahbahhar <129052493+sarahbahhar@users.noreply.github.com>
2023-04-23 20:57:15 +0200

    Update validerrecette.php

--- validerrecette.php
+++ validerrecette.php
@@ -5,7 +5,7 @@
     $bdd = new PDO("mysql:host=$servername;dbname=miam;", $username, $password);
     if(isset($_GET['valid']) AND !empty($_GET['valid'])){
         $valid_id=$_GET['valid'];
-        $valid=$bdd->prepare('UPDATE form_recette SET traitement= "Validé" WHERE id=?');
+        $valid=$bdd->prepare('UPDATE form_recette SET traitement= "Validé" WHERE id=?');//Cette requete va permettre a l'admin de validé la recette
         $valid->execute(array($valid_id));
        header('Location:listerecette.php');}
 ?>
