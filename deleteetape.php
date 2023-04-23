7d6caf2a5528aa3e38e48e7d0de70e4388b2b4d4
sarahbahhar <129052493+sarahbahhar@users.noreply.github.com>
2023-04-23 19:50:31 +0200

    Update deleteetape.php

--- deleteetape.php
+++ deleteetape.php
@@ -1,9 +1,11 @@
 <?php
-    require('header.php');   
+    require('header.php'); 
+    // on se connecte a la base de donnée
     $servername ="localhost";
     $username ="root";
     $password = "";
     $bdd = new PDO("mysql:host=$servername;dbname=miam;", $username, $password);
+    //si le bouton supprimer est pressé par l'admin on supprime l'etape de la base de donnee
     if(isset($_GET['delete']) AND !empty($_GET['delete'])){
         $delete_id=$_GET['delete'];
         $delete=$bdd->prepare('DELETE FROM form_etape WHERE id=?');
