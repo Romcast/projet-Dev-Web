0d61592de7a54eb79c9783564213599522e5723f
sarahbahhar <129052493+sarahbahhar@users.noreply.github.com>
2023-04-23 19:51:15 +0200

    Update deleteingredient.php

--- deleteingredient.php
+++ deleteingredient.php
@@ -4,6 +4,7 @@
     $username ="root";
     $password = "";
     $bdd = new PDO("mysql:host=$servername;dbname=miam;", $username, $password);
+    //si le bouton supprimer est pressé par l'admin on supprime l'ingredient de la base de donnee
     if(isset($_GET['delete']) AND !empty($_GET['delete'])){
         $delete_id=$_GET['delete'];
         $delete=$bdd->prepare('DELETE FROM form_ingredient WHERE id=?');
